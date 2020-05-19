98c9056fb73251a2976b37f633ef9f05d38bbe09
icepol <pavel.koci@icepol.net>
2020-05-19 08:21:15 +0200

    Delay the net animation

---
+++ Assets/Scripts/DelayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.enabled = false;
+
+        StartCoroutine(WaitAndAnimate());
+    }
+
+    IEnumerator WaitAndAnimate()
+    {
+        yield return new WaitForSeconds(Random.Range(0, 0.5f));
+
+        _animator.enabled = true;
+    }
+}
